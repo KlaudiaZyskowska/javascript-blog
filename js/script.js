92182166fe4f7b244d1c206a07e25d264b55abf4
Klaudia Zyskowska <klaudiazzyskowska@gmail.com>
2021-01-04 00:14:59 +0100

    Add const 'clickedElement' to script.js file

--- js/script.js
+++ js/script.js
@@ -4,19 +4,27 @@
   }); */
 
 const titleClickHandler = function () {
+    event.preventDefault();
     console.log('Link was clicked!');
     console.log(event);
 
-    /* remove class 'active' from all article links  */
+    /* [DONE] remove class 'active' from all article links  */
     const activeLinks = document.querySelectorAll('.titles a.active');
 
     for (let activeLink of activeLinks) {
         activeLink.classList.remove('active');
     }
 
-    /* add class 'active' to the clicked link */
+    /* [DONE] add class 'active' to the clicked link */
+    event.preventDefault();
 
-    /* remove class 'active' from all articles */
+    const clickedElement = this;
+
+    clickedElement.classList.add('active');
+
+    console.log('clickedElement:', clickedElement);
+
+    /* [DONE] remove class 'active' from all articles */
     const activeArticles = document.querySelectorAll('.posts article.active');
 
     for (let activeArticle of activeArticles) {
